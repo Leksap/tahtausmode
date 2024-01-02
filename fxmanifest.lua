8a9be9a05c589a8cdb1bf0d51b93e9dc2b9df56b
Leka <56492116+Leksap@users.noreply.github.com>
2024-01-02 23:22:17 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,16 +2,15 @@ fx_version 'cerulean'
 game 'gta5'
 author 'Leksa'
 description 'Unityn tähtäysmode'
-version '1.0.0'
+version '1.0.1'
 
 lua54 'yes'
 
 shared_scripts {
     '@ox_lib/init.lua'
 }
-client_scripts {
-    'tahtausmode.lua',
-}
+
+client_script 'tahtausmode.lua'
 server_script 'update.lua'
 
 dependency 'ox_lib'
