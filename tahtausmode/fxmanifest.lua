0ab4211ed199919dbe6e92a6ca1202a0d8664504
Leka <56492116+Leksap@users.noreply.github.com>
2024-01-02 17:14:42 +0200

    Add files via upload

---
+++ tahtausmode/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Leksa'
+description 'Unityn tähtäysmode'
+
+lua54 'yes'
+
+shared_scripts {
+    '@ox_lib/init.lua'
+}
+client_scripts {
+    'tahtausmode.lua',
+}
+
+
+dependency 'ox_lib'
